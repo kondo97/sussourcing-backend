5a9152c42178120dc9819be3b7612e0bc4cb7ea2
kondo97 <kondo9523@gmail.com>
2022-05-29 23:10:04 +0900

    deviseの導入

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,9 +60,11 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "ok_now_production"
+  # config.active_job.queue_name_prefix = "knockwork_production"
 
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.default_url_options = { host: 'knockwork.herokuapp.com', port: 80 }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
